f176180f1c97fd4af62e61f717c75a9ccd607ee8
JalalJanKhan <jalalkhan1193@gmail.com>
2020-06-09 02:17:29 +0500

    updated

--- fbauth/src/main/java/com/thesoftparrot/fbauth/AuthBaseActivity.java
+++ fbauth/src/main/java/com/thesoftparrot/fbauth/AuthBaseActivity.java
@@ -2,11 +2,15 @@ package com.thesoftparrot.fbauth;
 
 import android.content.Intent;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -47,6 +51,96 @@ public abstract class AuthBaseActivity extends AppCompatActivity {
                         .setAvailableProviders(mEmailProvider)
                         .build(),
                 REQUEST_CODE_EMAIL_AUTH);
+    }
+
+    protected void signInByEmailAndPassword(String email, String password){
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+
+        auth
+                .signInWithEmailAndPassword(email, password)
+                .addOnSuccessListener(authResult -> {
+                    if(authResult != null){
+                        FirebaseUser user = authResult.getUser();
+
+                        if(user != null){
+                            String userId = user.getUid();
+                            authSuccessful(userId);
+                        }
+                    }else {
+                        authFailed("Unable to authenticate via given email and password");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        authFailed(e.getMessage());
+                    }
+                });
+    }
+
+    protected String getUserId(){
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser user = auth.getCurrentUser();
+
+        if(user != null)
+            return user.getUid();
+
+        authFailed("Please SignIn to proceed...");
+
+        return null;
+    }
+
+    protected void signOut(Class target){
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        auth.signOut();
+
+        Intent intent = new Intent(AuthBaseActivity.this, target);
+        startActivity(intent);
+        finish();
+    }
+
+    protected void registerNewUser(String email, String password){
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        auth
+                .createUserWithEmailAndPassword(email, password)
+                .addOnSuccessListener(authResult -> {
+                    if(authResult != null){
+                        FirebaseUser user = authResult.getUser();
+
+                        if(user != null){
+                            String userId = user.getUid();
+                            authSuccessful(userId);
+                        }
+                    }else {
+                        authFailed("Unable to Register via given email and password");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        authFailed(e.getMessage());
+                    }
+                });
+    }
+
+    protected void signInAnonymously(){
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        auth
+                .signInAnonymously()
+                .addOnSuccessListener(authResult -> {
+                    if(authResult != null){
+                        FirebaseUser user = authResult.getUser();
+
+                        if(user != null){
+                            String userId = user.getUid();
+                            authSuccessful(userId);
+                        }
+                    }else {
+                        authFailed("Unable to Authenticate Anonymously");
+                    }
+                })
+                .addOnFailureListener(e -> authFailed(e.getMessage()));
     }
 
     @Override
--- fbauth/src/main/java/com/thesoftparrot/fbauth/AuthBaseFragment.java
+++ fbauth/src/main/java/com/thesoftparrot/fbauth/AuthBaseFragment.java
@@ -3,11 +3,13 @@ package com.thesoftparrot.fbauth;
 import android.app.Activity;
 import android.content.Intent;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -51,6 +53,95 @@ public abstract class AuthBaseFragment extends Fragment {
                         .setAvailableProviders(mEmailProvider)
                         .build(),
                 REQUEST_CODE_EMAIL_AUTH);
+    }
+
+    protected void signInByEmailAndPassword(String email, String password){
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+
+        auth
+                .signInWithEmailAndPassword(email, password)
+                .addOnSuccessListener(authResult -> {
+                    if(authResult != null){
+                        FirebaseUser user = authResult.getUser();
+
+                        if(user != null){
+                            String userId = user.getUid();
+                            authSuccessful(userId);
+                        }
+                    }else {
+                        authFailed("Unable to authenticate via given email and password");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        authFailed(e.getMessage());
+                    }
+                });
+    }
+
+    protected void registerNewUser(String email, String password){
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        auth
+                .createUserWithEmailAndPassword(email, password)
+                .addOnSuccessListener(authResult -> {
+                    if(authResult != null){
+                        FirebaseUser user = authResult.getUser();
+
+                        if(user != null){
+                            String userId = user.getUid();
+                            authSuccessful(userId);
+                        }
+                    }else {
+                        authFailed("Unable to Register via given email and password");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        authFailed(e.getMessage());
+                    }
+                });
+    }
+
+    protected String getUserId(){
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser user = auth.getCurrentUser();
+
+        if(user != null)
+            return user.getUid();
+
+        authFailed("Please SignIn to proceed...");
+
+        return null;
+    }
+
+    protected void signOut(Class target){
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        auth.signOut();
+
+        Intent intent = new Intent(getContext(), target);
+        startActivity(intent);
+    }
+
+    protected void signInAnonymously(){
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        auth
+                .signInAnonymously()
+                .addOnSuccessListener(authResult -> {
+                    if(authResult != null){
+                        FirebaseUser user = authResult.getUser();
+
+                        if(user != null){
+                            String userId = user.getUid();
+                            authSuccessful(userId);
+                        }
+                    }else {
+                        authFailed("Unable to Authenticate Anonymously");
+                    }
+                })
+                .addOnFailureListener(e -> authFailed(e.getMessage()));
     }
 
     @Override
